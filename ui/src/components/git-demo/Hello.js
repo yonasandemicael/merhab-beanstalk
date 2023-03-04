62e000ad704c670e564834c3a52af41ee1ee6e0d
Yonas <yandemicael@gmail.com>
2023-03-04 15:23:22 -0600

    commit-2

--- ui/src/components/git-demo/Hello.js
+++ ui/src/components/git-demo/Hello.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export const Hello = ({ children }) => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <h1>Welcome </h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
+        cupiditate repudiandae ipsam iure fuga animi praesentium quisquam vel
+        veritatis fugit.
+      </p>
+    </>
+  );
 };
